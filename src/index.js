7d633c83f45491dc4e16ad1326a0a5e81bc39f1f
charlieperson <cperson@thoughtworks.com>
2017-06-16 10:36:07 -0500

    Complete basic search functionality with onlyInStock check working correctly

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-var products = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-];
-
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this)
+    this.handleInStockInputChange = this.handleInStockInputChange.bind(this)
+  }
+
+  handleFilterTextInputChange(e) {
+    this.props.onFilterTextInput(e.target.value)
+  }
+
+  handleInStockInputChange(e) {
+    this.props.onInStockInput(e.target.checked)
+  }
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Search..." value={this.props.filterText}/>
+        <input type="text" placeholder="Search..." value={this.props.filterText} onChange={this.handleFilterTextInputChange}/>
         <p>
-          <input type="checkbox" />
+          <input type="checkbox" checked={this.props.inStockOnly} onChange={this.handleInStockInputChange}/>
           {' '}
           Only show products in stock
         </p>
@@ -47,7 +51,7 @@ class ProductTable extends React.Component {
   render() {
     var currentCategory = null
     var rows = []
-    var productArray = products.forEach((product) => {
+    var productArray = this.props.products.forEach((product) => {
       if(!product.stocked && this.props.inStockOnly || this.props.filterText.indexOf(product.name) === -1) { return }
 
       if(product.category !== currentCategory) {
@@ -75,22 +79,56 @@ class ProductTable extends React.Component {
 class FilterableProductTable extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {filterText: 'Basketball', inStockOnly: false}
+    this.state = {filterText: '',
+                  inStockOnly: false}
+
+    this.handleFilterTextInput = this.handleFilterTextInput.bind(this)
+    this.handleInStockInput = this.handleInStockInput.bind(this)
+  }
+
+  handleFilterTextInput(filterText) {
+    this.setState({
+      filterText: filterText
+    })
+  }
+
+  handleInStockInput(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    })
   }
 
   render() {
     return (
       <div>
-        <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} />
-        <ProductTable filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} />
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.inStockOnly}
+          onFilterTextInput={this.handleFilterTextInput}
+          onInStockInput={this.handleInStockInput}
+        />
+        <ProductTable
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          products={this.props.products}
+        />
       </div>
     );
   }
 }
 
+var PRODUCTS = [
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
+
 // ========================================
 
 ReactDOM.render(
-  <FilterableProductTable />,
+  <FilterableProductTable products={PRODUCTS} />,
   document.getElementById('root')
 );
